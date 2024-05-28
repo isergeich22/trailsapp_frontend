8588602a23d882d5f259539b9b7937a2aafb876e
isergeich22 <skiing1r@gmail.com>
2024-05-28 20:17:58 +0300

    update commit

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({  
   server: {
-    port: 80,
-    proxy: {
-      '/api': {
-        target: 'https://80.90.178.41:8081',
-        changeOrigin: true,
-        secure: true
-      }
-    }
+    port: 80
   },
   plugins: [
     vue(),
