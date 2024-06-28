28b59fa4150f7a23901ca95a999b232ee72cd889
isergeich22 <skiing1r@gmail.com>
2024-06-28 21:05:27 +0300

    added pagination for history

--- src/components/HistoryView.vue
+++ src/components/HistoryView.vue
@@ -1,13 +1,28 @@
 <script>
     import axios from 'axios'
 
+    function chunckArray(array, chunkSize) {
+                
+        const result = []
+        for(let i = 0; i < array.length; i += chunkSize) {
+            const chunk = array.slice(i, i + chunkSize)
+            result.push(chunk)
+        }
+        return result
+
+    }
+
     export default {
         data() {
             return {
                 baseApiUrl: import.meta.env.VITE_BASE_API_URL,
                 routes: [],
                 message: '',
-                toggleList: []
+                toggleList: [],
+                paginatedRoutes: [],
+                routesPerPage: 10,
+                currentPage: 1,
+                totalPages: 0
             }
         },
 
@@ -19,7 +34,9 @@
                 this.routes = response.data
                 this.routes.forEach(el => {
                     this.toggleList.push(false)
-                })
+                })                
+                this.paginatedRoutes = chunckArray(this.routes, this.routesPerPage)
+                this.totalPages = this.paginatedRoutes.length
             }
 
             if(typeof response.data === 'string') {
@@ -31,6 +48,23 @@
         methods: {
             toggleIt(i) {
                 this.toggleList[i] = !this.toggleList[i]
+            },
+            prevPage() {
+                // document.getElementById(`${this.currentPage}`).classList.remove('teal')
+                // document.getElementById(`${this.currentPage}`).classList.remove('lighten-1')
+                this.currentPage--
+                // document.getElementById(`${this.currentPage}`).classList.add('teal')
+                // document.getElementById(`${this.currentPage}`).classList.add('lighten-1')
+            },
+            nextPage() {
+                // document.getElementById(`${this.currentPage}`).classList.remove('teal')
+                // document.getElementById(`${this.currentPage}`).classList.remove('lighten-1')
+                this.currentPage++
+                // document.getElementById(`${this.currentPage}`).classList.add('teal')
+                // document.getElementById(`${this.currentPage}`).classList.add('lighten-1')
+            },
+            changePage(pageNumber) {
+                this.currentPage = pageNumber
             }
         }
     }
@@ -43,7 +77,7 @@
                 <h3>История маршрутов</h3>
             </div>
         </div>
-        <template v-for="(route, idx) in routes">
+        <template v-for="(route, idx) in paginatedRoutes[currentPage - 1]">
             <div class="row">            
                 <div class="col s12 teal lighten-2 toggle" @click="toggleIt(idx)">
                     <p class="white-text"><b>{{ route.executorName }} - {{ route.routeDate }}</b></p>
@@ -106,6 +140,16 @@
                 </div>
             </div>
         </template>
+        <div class="row flex-row">
+            <button class="waves-effect waves-teal btn-flat" :disabled="currentPage <= 1" @click="prevPage"><i class="material-icons">chevron_left</i></button>
+            <ul class="pagination">
+                <li class="waves-effect" v-for="(elem, idx) in paginatedRoutes" :id="idx+1">
+                    <button v-if="idx+1 === currentPage" class="waves-effect waves-teal btn-flat teal lighten-1 white-text" @click="changePage(idx+1)">{{ idx + 1 }}</button>
+                    <button v-else class="waves-effect waves-teal btn-flat" @click="changePage(idx+1)">{{ idx + 1 }}</button>
+                </li>
+            </ul>
+            <button class="waves-effect waves-teal btn-flat" :disabled="currentPage >= totalPages" @click="nextPage"><i class="material-icons">chevron_right</i></button>
+        </div>
     </template>
     <template v-else>
         <div class="row">
@@ -117,6 +161,14 @@
 </template>
 
 <style>
+
+    .pagination {
+        margin: 0;
+    }
+
+        .pagination li {
+            height: 35px;
+        }
 
     .toggle {
         cursor: pointer;
